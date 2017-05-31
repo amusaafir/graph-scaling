91ac5a8bfd97fb44fc63a8b89c9582a3750b4c9d
Ahmed <amusaafir@users.noreply.github.com>
2017-05-31 18:11:29 +0200

    Added CSR_List struct

--- GraphShrinkingExpanding/kernel.cu
+++ GraphShrinkingExpanding/kernel.cu
@@ -55,8 +55,8 @@ ANALYSIS
 #define SIZE_EDGES 2312497
 
 // Pokec relationships
-//#define SIZE_VERTICES 1632803
-//#define SIZE_EDGES 30622564 
+#define SIZE_VERTICES 1632803
+#define SIZE_EDGES 30622564 
 
 // Edge list example
 //#define SIZE_VERTICES 6
@@ -86,6 +86,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
 
 typedef struct Sampled_Vertices sampled_vertices;
 typedef struct COO_List coo_list;
+typedef struct CSR_List csr_list;
 typedef struct Edge edge;
 typedef struct Sampled_Graph_Version;
 typedef struct Bridge_Edge;
@@ -101,7 +102,7 @@ void print_debug_log(char*, int);
 void print_coo(int*, int*);
 void print_csr(int*, int*);
 void sample_graph(char*, char*, float);
-void convert_coo_to_csr_format(int*, int*, int*, int*);
+CSR_List* convert_coo_to_csr_format(int*, int*);
 void expand_graph(char*, char*, float);
 void link_using_star_topology(Sampled_Graph_Version*, int, std::vector<Bridge_Edge>&);
 void add_edge_interconnection_between_graphs(int, Sampled_Graph_Version*, Sampled_Graph_Version*, std::vector<Bridge_Edge>&);
@@ -114,6 +115,11 @@ typedef struct COO_List {
 	int* source;
 	int* destination;
 } COO_List;
+
+typedef struct CSR_List {
+	int* offsets;
+	int* indices;
+} CSR_List;
 
 typedef struct Sampled_Vertices {
 	int* vertices;
@@ -142,7 +148,8 @@ __device__ int push_edge(Edge &edge) {
 	if (edge_index < SIZE_EDGES) {
 		edge_data[edge_index] = edge;
 		return edge_index;
-	} else {
+	}
+	else {
 		printf("Maximum edge size threshold reached.");
 		return -1;
 	}
@@ -198,13 +205,13 @@ TODO: Allocate the memory on the GPU only when you need it, after collecting the
 int main() {
 	//char* input_path = "C:\\Users\\AJ\\Documents\\example_graph.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\nvgraphtest\\nvGraphExample-master\\nvGraphExample\\web-Stanford.txt";
-	char* input_path = "C:\\Users\\AJ\\Desktop\\nvgraphtest\\nvGraphExample-master\\nvGraphExample\\web-Stanford_large.txt";
+	//char* input_path = "C:\\Users\\AJ\\Desktop\\nvgraphtest\\nvGraphExample-master\\nvGraphExample\\web-Stanford_large.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\edge_list_example.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\roadnet.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\new_datasets\\facebook_graph.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\output_test\\social\\soc-pokec-relationships.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\new_datasets\\roadNet-PA.txt";
-	//char* input_path = "C:\\Users\\AJ\\Desktop\\new_datasets\\soc-pokec-relationships.txt";
+	char* input_path = "C:\\Users\\AJ\\Desktop\\new_datasets\\soc-pokec-relationships.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\new_datasets\\com-orkut.ungraph.txt";
 	//char* input_path = "C:\\Users\\AJ\\Desktop\\new_datasets\\soc-LiveJournal1.txt";
 
@@ -213,24 +220,19 @@ int main() {
 	//sample_graph(input_path, output_path, 0.5);
 
 	expand_graph(input_path, output_path, 3);
-	
+
 	return 0;
 }
 
 void sample_graph(char* input_path, char* output_path, float fraction) {
 	std::vector<int> source_vertices;
 	std::vector<int> destination_vertices;
+
+	// Convert edge list to COO
 	COO_List* coo_list = load_graph_from_edge_list_file_to_coo(source_vertices, destination_vertices, input_path);
 
-	// print_coo(source_vertices, target_vertices);
-
-	// Convert the COO graph into a CSR format for the in memory GPU representation
-	int* h_offsets = (int*)malloc((SIZE_VERTICES + 1) * sizeof(int));
-	int* h_indices = (int*)malloc(SIZE_EDGES * sizeof(int));
-
-	convert_coo_to_csr_format(coo_list->source, coo_list->destination, h_offsets, h_indices);
-
-	//print_csr(h_offsets, h_indices);
+	// Convert the COO graph into a CSR format (for the in-memory GPU representation) 
+	CSR_List* csr_list = convert_coo_to_csr_format(coo_list->source, coo_list->destination);
 
 	// Edge based Node Sampling Step
 	Sampled_Vertices* sampled_vertices = perform_edge_based_node_sampling_step(coo_list->source, coo_list->destination, fraction);
@@ -241,8 +243,8 @@ void sample_graph(char* input_path, char* output_path, float fraction) {
 	int* d_indices;
 	cudaMalloc((void**)&d_offsets, sizeof(int)*(SIZE_VERTICES + 1));
 	cudaMalloc((void**)&d_indices, sizeof(int)*SIZE_EDGES);
-	gpuErrchk(cudaMemcpy(d_indices, h_indices, SIZE_EDGES * sizeof(int), cudaMemcpyHostToDevice));
-	gpuErrchk(cudaMemcpy(d_offsets, h_offsets, sizeof(int)*(SIZE_VERTICES + 1), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_indices, csr_list->indices, SIZE_EDGES * sizeof(int), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_offsets, csr_list->offsets, sizeof(int)*(SIZE_VERTICES + 1), cudaMemcpyHostToDevice));
 
 	int* d_sampled_vertices;
 	gpuErrchk(cudaMalloc((void**)&d_sampled_vertices, sizeof(int)*SIZE_VERTICES));
@@ -270,17 +272,23 @@ void sample_graph(char* input_path, char* output_path, float fraction) {
 	// Cleanup
 	free(sampled_vertices->vertices);
 	free(sampled_vertices);
+
 	free(coo_list);
-	free(h_indices);
-	free(h_offsets);
+
+	free(csr_list->indices);
+	free(csr_list->offsets);
+	free(csr_list);
 }
 
 /*
 Fast conversion to CSR - Using nvGraph for conversion
 Modified from: github.com/bmass02/nvGraphExample
 */
-void convert_coo_to_csr_format(int* source_vertices, int* target_vertices, int* h_offsets, int* h_indices) {
+CSR_List* convert_coo_to_csr_format(int* source_vertices, int* target_vertices) {
 	printf("\nConverting COO to CSR format.");
+	CSR_List* csr_list = (CSR_List*)malloc(sizeof(CSR_List));
+	csr_list->offsets = (int*)malloc((SIZE_VERTICES + 1) * sizeof(int));
+	csr_list->indices = (int*)malloc(SIZE_EDGES * sizeof(int));
 
 	// First setup the COO format from the input (source_vertices and target_vertices array)
 	nvgraphHandle_t handle;
@@ -305,7 +313,7 @@ void convert_coo_to_csr_format(int* source_vertices, int* target_vertices, int* 
 	gpuErrchk(cudaMalloc((void**)&d_edge_data, sizeof(float) * SIZE_EDGES)); // Note: only allocate this for 1 float since we don't have any data yet
 	gpuErrchk(cudaMalloc((void**)&d_destination_edge_data, sizeof(float) * SIZE_EDGES)); // Note: only allocate this for 1 float since we don't have any data yet
 
-	// Convert COO to a CSR format
+																						 // Convert COO to a CSR format
 	nvgraphCSRTopology32I_t csrTopology = (nvgraphCSRTopology32I_t)malloc(sizeof(struct nvgraphCSRTopology32I_st));
 	int **d_indices = &(csrTopology->destination_indices);
 	int **d_offsets = &(csrTopology->source_offsets);
@@ -318,8 +326,8 @@ void convert_coo_to_csr_format(int* source_vertices, int* target_vertices, int* 
 	gpuErrchk(cudaPeekAtLastError());
 
 	// Copy data to the host (without edge data)
-	gpuErrchk(cudaMemcpy(h_indices, *d_indices, SIZE_EDGES * sizeof(int), cudaMemcpyDeviceToHost));
-	gpuErrchk(cudaMemcpy(h_offsets, *d_offsets, (SIZE_VERTICES + 1) * sizeof(int), cudaMemcpyDeviceToHost));
+	gpuErrchk(cudaMemcpy(csr_list->indices, *d_indices, SIZE_EDGES * sizeof(int), cudaMemcpyDeviceToHost));
+	gpuErrchk(cudaMemcpy(csr_list->offsets, *d_offsets, (SIZE_VERTICES + 1) * sizeof(int), cudaMemcpyDeviceToHost));
 
 	// Clean up (Data allocated on device and both topologies, since we only want to work with indices and offsets for now)
 	cudaFree(d_indices);
@@ -330,6 +338,8 @@ void convert_coo_to_csr_format(int* source_vertices, int* target_vertices, int* 
 	cudaFree(cooTopology->source_indices);
 	free(cooTopology);
 	free(csrTopology);
+
+	return csr_list;
 }
 
 int get_thread_size() {
@@ -386,7 +396,7 @@ COO_List* load_graph_from_edge_list_file_to_coo(std::vector<int>& source_vertice
 	std::unordered_map<int, int> map_from_edge_to_coordinate;
 
 	FILE* file = fopen(file_path, "r");
-	
+
 	char line[256];
 
 	int current_coordinate = 0;
@@ -420,7 +430,7 @@ COO_List* load_graph_from_edge_list_file_to_coo(std::vector<int>& source_vertice
 
 	// Print edges
 	/*for (int i = 0; i < source_vertices.size(); i++) {
-		printf("\n(%d, %d)", coo_list->source[i], coo_list->destination[i]);
+	printf("\n(%d, %d)", coo_list->source[i], coo_list->destination[i]);
 	}*/
 
 	fclose(file);
@@ -433,7 +443,8 @@ int add_vertex_as_coordinate(std::vector<int>& vertices_type, std::unordered_map
 		vertices_type.push_back(map_from_edge_to_coordinate.at(vertex));
 
 		return coordinate;
-	} else {
+	}
+	else {
 		map_from_edge_to_coordinate[vertex] = coordinate;
 		vertices_type.push_back(coordinate);
 		coordinate++;
@@ -445,19 +456,19 @@ int add_vertex_as_coordinate(std::vector<int>& vertices_type, std::unordered_map
 Sampled_Vertices* perform_edge_based_node_sampling_step(int* source_vertices, int* target_vertices, float fraction) {
 	printf("\nPerforming edge based node sampling step.\n");
 
-	Sampled_Vertices* sampled_vertices = (Sampled_Vertices*) malloc(sizeof(Sampled_Vertices));
+	Sampled_Vertices* sampled_vertices = (Sampled_Vertices*)malloc(sizeof(Sampled_Vertices));
 
 	int amount_total_sampled_vertices = calculate_node_sampled_size(fraction);
 
 	std::random_device seeder;
 	std::mt19937 engine(seeder());
 
-	sampled_vertices->vertices = (int*) calloc(SIZE_VERTICES, sizeof(int));
+	sampled_vertices->vertices = (int*)calloc(SIZE_VERTICES, sizeof(int));
 	int collected_amount = 0;
 
 	while (collected_amount < amount_total_sampled_vertices) {
 		// Pick a random vertex u
-		std::uniform_int_distribution<int> range_edges(0, (SIZE_EDGES-1)); // Don't select the last element in the offset
+		std::uniform_int_distribution<int> range_edges(0, (SIZE_EDGES - 1)); // Don't select the last element in the offset
 		int random_edge_index = range_edges(engine);
 
 		// Insert u, v (TODO: extract to method per vertex)
@@ -475,6 +486,8 @@ Sampled_Vertices* perform_edge_based_node_sampling_step(int* source_vertices, in
 
 	sampled_vertices->sampled_vertices_size = collected_amount;
 
+	printf("\nDone with node sampling step..");
+
 	return sampled_vertices;
 }
 
@@ -488,21 +501,15 @@ void expand_graph(char* input_path, char* output_path, float scaling_factor) {
 	std::vector<int> source_vertices;
 	std::vector<int> destination_vertices;
 	COO_List* coo_list = load_graph_from_edge_list_file_to_coo(source_vertices, destination_vertices, input_path);
-
-	// Convert the COO graph into a CSR format for the in memory GPU representation
-	int* h_offsets = (int*)malloc((SIZE_VERTICES + 1) * sizeof(int));
-	int* h_indices = (int*)malloc(SIZE_EDGES * sizeof(int));
-
-	convert_coo_to_csr_format(coo_list->source, coo_list->destination, h_offsets, h_indices);
+	CSR_List* csr_list = convert_coo_to_csr_format(coo_list->source, coo_list->destination);
 
 	const int amount_of_sampled_graphs = scaling_factor / DEFAULT_EXPANDING_SAMPLE_SIZE;
+	printf("Amount of sampled graph versions: %d", amount_of_sampled_graphs);
 
-	printf("Amount of sampled graphs: %d", amount_of_sampled_graphs);
+	Sampled_Vertices** sampled_vertices_per_graph = (Sampled_Vertices**)malloc(sizeof(Sampled_Vertices)*amount_of_sampled_graphs);
 
-	Sampled_Vertices** sampled_vertices_per_graph = (Sampled_Vertices**) malloc(sizeof(Sampled_Vertices)*amount_of_sampled_graphs);
-	
-	int** d_size_edges = (int**) malloc(sizeof(int*)*amount_of_sampled_graphs);
-	Edge** d_edge_data_expanding = (Edge**) malloc(sizeof(Edge*)*amount_of_sampled_graphs);
+	int** d_size_edges = (int**)malloc(sizeof(int*)*amount_of_sampled_graphs);
+	Edge** d_edge_data_expanding = (Edge**)malloc(sizeof(Edge*)*amount_of_sampled_graphs);
 
 	Sampled_Graph_Version* sampled_graph_version_list = new Sampled_Graph_Version[amount_of_sampled_graphs];
 	char current_label = 'a';
@@ -515,11 +522,9 @@ void expand_graph(char* input_path, char* output_path, float scaling_factor) {
 	for (int i = 0; i < amount_of_sampled_graphs; i++) {
 		sampled_vertices_per_graph[i] = perform_edge_based_node_sampling_step(coo_list->source, coo_list->destination, DEFAULT_EXPANDING_SAMPLE_SIZE);
 		printf("\nCollected %d vertices.", sampled_vertices_per_graph[i]->sampled_vertices_size);
-		printf("\nDone with node sampling step..");
-		// Induction step (TODO: re-use device memory from CSR conversion)
-		
-		gpuErrchk(cudaMemcpy(d_indices, h_indices, SIZE_EDGES * sizeof(int), cudaMemcpyHostToDevice));
-		gpuErrchk(cudaMemcpy(d_offsets, h_offsets, sizeof(int)*(SIZE_VERTICES + 1), cudaMemcpyHostToDevice));
+
+		gpuErrchk(cudaMemcpy(d_indices, csr_list->indices, SIZE_EDGES * sizeof(int), cudaMemcpyHostToDevice));
+		gpuErrchk(cudaMemcpy(d_offsets, csr_list->offsets, sizeof(int)*(SIZE_VERTICES + 1), cudaMemcpyHostToDevice));
 
 		int* d_sampled_vertices;
 		gpuErrchk(cudaMalloc((void**)&d_sampled_vertices, sizeof(int)*SIZE_VERTICES));
@@ -530,12 +535,12 @@ void expand_graph(char* input_path, char* output_path, float scaling_factor) {
 		gpuErrchk(cudaMemcpy(d_size_edges[i], &h_size_edges, sizeof(int), cudaMemcpyHostToDevice));
 
 		gpuErrchk(cudaMalloc((void**)&d_edge_data_expanding[i], sizeof(Edge)*SIZE_EDGES));
-		
+
 		cudaDeviceSynchronize(); // This can be deleted - double check
 
 		printf("\nRunning kernel (induction step) with block size %d and thread size %d:", get_block_size(), get_thread_size());
-		perform_induction_step_expanding <<<get_block_size(), get_thread_size()>>>(d_sampled_vertices, d_offsets, d_indices, d_edge_data_expanding[i], d_size_edges[i]);
-		
+		perform_induction_step_expanding << <get_block_size(), get_thread_size() >> >(d_sampled_vertices, d_offsets, d_indices, d_edge_data_expanding[i], d_size_edges[i]);
+
 		// Edge size
 		int h_size_edges_result;
 		gpuErrchk(cudaMemcpy(&h_size_edges_result, d_size_edges[i], sizeof(int), cudaMemcpyDeviceToHost));
@@ -555,7 +560,7 @@ void expand_graph(char* input_path, char* output_path, float scaling_factor) {
 
 		// Cleanup
 		delete(sampled_graph_version);
-		
+
 		cudaFree(d_sampled_vertices);
 		cudaFree(d_edge_data_expanding[i]);
 		cudaFree(d_size_edges);
@@ -567,15 +572,16 @@ void expand_graph(char* input_path, char* output_path, float scaling_factor) {
 	cudaFree(d_indices);
 	free(sampled_vertices_per_graph);
 	free(coo_list);
-	free(h_indices);
-	free(h_offsets);
+	free(csr_list->indices);
+	free(csr_list->offsets);
+	free(csr_list);
 
 	printf("\nAfter test: %d", sampled_graph_version_list[0].edges.size());
 
 	// For each sampled graph version, copy the data back to the host
 	std::vector<Bridge_Edge> bridge_edges;
 	link_using_star_topology(sampled_graph_version_list, amount_of_sampled_graphs, bridge_edges);
-	
+
 	write_expanded_output_to_file(sampled_graph_version_list, amount_of_sampled_graphs, bridge_edges, output_path);
 
 	// Cleanup
@@ -588,10 +594,10 @@ void link_using_star_topology(Sampled_Graph_Version* sampled_graph_version_list,
 	printf("\nAfter size now 1: %d with label: %c", sampled_graph_version_list[1].edges.size(), sampled_graph_version_list[1].label);
 	printf("\nAfter size now 2: %d with label: %c", sampled_graph_version_list[2].edges.size(), sampled_graph_version_list[2].label);
 	printf("\nAfter size now 3: %d with label: %c", sampled_graph_version_list[3].edges.size(), sampled_graph_version_list[3].label);*/
-	
+
 	// First sampled version will be the graph in the center
 	Sampled_Graph_Version center_graph = sampled_graph_version_list[0];
-	
+
 	int amount_of_edge_interconnections = 1;
 	for (int i = 1; i < amount_of_sampled_graphs; i++) { // Skip the center graph 
 		add_edge_interconnection_between_graphs(amount_of_edge_interconnections, &(sampled_graph_version_list[i]), &center_graph, bridge_edges);
@@ -609,7 +615,7 @@ void add_edge_interconnection_between_graphs(int amount_of_edge_interconnections
 	for (int i = 0; i < amount_of_edge_interconnections; i++) {
 		int vertex_a = select_random_bridge_vertex(graph_a);
 		int vertex_b = select_random_bridge_vertex(graph_b);
-		
+
 		// TODO: Extract function
 		// Add edge
 		Bridge_Edge bridge_edge;
@@ -633,6 +639,8 @@ int select_random_bridge_vertex(Sampled_Graph_Version* graph) {
 }
 
 void write_expanded_output_to_file(Sampled_Graph_Version* sampled_graph_version_list, int amount_of_sampled_graphs, std::vector<Bridge_Edge>& bridge_edges, char* ouput_path) {
+	printf("\nWriting results to output file.");
+
 	char* file_path = ouput_path;
 	FILE *output_file = fopen(file_path, "w");
 
@@ -647,15 +655,17 @@ void write_expanded_output_to_file(Sampled_Graph_Version* sampled_graph_version_
 			fprintf(output_file, "\n%c%d\t%c%d", sampled_graph_version_list[i].label, sampled_graph_version_list[i].edges[p].source, sampled_graph_version_list[i].label, sampled_graph_version_list[i].edges[p].destination);
 		}
 	}
-	
+
 	for (int i = 0; i < bridge_edges.size(); i++) {
 		fprintf(output_file, "\n%s\t%s", bridge_edges[i].source, bridge_edges[i].destination);
 	}
-		
+
 	fclose(output_file);
 }
 
 void write_output_to_file(std::vector<Edge>& results, char* ouput_path) {
+	printf("\nWriting results to output file.");
+	
 	char* file_path = ouput_path;
 	FILE *output_file = fopen(file_path, "w");
 
@@ -694,7 +704,7 @@ void check(nvgraphStatus_t status) {
 		printf("\nError converting to CSR: %d - NVGRAPH_STATUS_NOT_INITIALIZED", status);
 		exit(0);
 	}
-	else if (status== NVGRAPH_STATUS_ALLOC_FAILED) {
+	else if (status == NVGRAPH_STATUS_ALLOC_FAILED) {
 		printf("\nError converting to CSR: %d - NVGRAPH_STATUS_ALLOC_FAILED", status);
 		exit(0);
 	}
@@ -706,7 +716,7 @@ void check(nvgraphStatus_t status) {
 		printf("\nError converting to CSR: %d - NVGRAPH_STATUS_INTERNAL_ERROR", status);
 		exit(0);
 	}
-	else if(status == NVGRAPH_STATUS_SUCCESS) {
+	else if (status == NVGRAPH_STATUS_SUCCESS) {
 		printf("\nConverted to CSR successfully (statuscode %d).\n", status);
 	}
 	else {
