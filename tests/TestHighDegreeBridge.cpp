5ff877160b8d9cf7e471e2998277c76ac1709b61
AJ <ahmedmusaafir@gmail.com>
2019-04-03 15:22:08 +0200

    Add remainder tests

--- tests/TestHighDegreeBridge.cpp
+++ tests/TestHighDegreeBridge.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include "../scaling/scale-up/bridge/HighDegreeBridge.h"
 
-TEST(HighDegreeBridge, justATest) {
+TEST(HighDegreeBridge, checkName) {
     HighDegreeBridge* hdb = new HighDegreeBridge(10, false);
 
     ASSERT_EQ(hdb->getName(), "HighDegreeBridge");
