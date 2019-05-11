e4637ff279f498895562f1cb5635207816040868
amusaafir <ahmedmusaafir@gmail.com>
2019-05-11 15:10:17 +0200

    Pick random vertex if all the vertices in a disconnected component have been visited

--- scaling/scale-down/ForestFire.cpp
+++ scaling/scale-down/ForestFire.cpp
@@ -27,62 +27,58 @@ Graph* ForestFire::sample(float fraction) {
     const bool enableJumpWithInterconnection = true;
     const bool isUndirected = true;
 
-    // If the sourceVertex is not given, pick a random one. This will hit any disconnected component eventually.
-    if (sourceVertex == -1) {
-        sourceVertex = graph->getEdges()[getRandomIntBetweenRange(0, graph->getEdges().size() - 1)].getSource();
-    }
 
-    assert(sourceVertex != -1);
-
-    std::queue<long long> queue;
+    bool reachedDesiredNumberOfEdges = false;
 
     std::unordered_set<long long> sampledVertices;
     std::vector<Edge<long long>> sampledEdges;
 
-    queue.push(sourceVertex);
 
-    long long desiredNumberOfEdges = graph->getEdges().size() * fraction;
-    std::unordered_map<long long, std::vector<long long>> adjacencyList = graph->getAdjacencyList();
-
-    assert(!queue.empty());
-
-    bool reachedDesiredNumberOfEdges = false;
-
-    while (!queue.empty() && !reachedDesiredNumberOfEdges) {
-        long long sourceVertex = queue.front();
-        queue.pop();
-
-
-        if (sampledVertices.count(sourceVertex)) {
-            continue;
+    while (!reachedDesiredNumberOfEdges) {
+        // If the sourceVertex is not given, pick a random one. This will hit any disconnected component eventually.
+        if (sourceVertex == -1) {
+            sourceVertex = graph->getEdges()[getRandomIntBetweenRange(0, graph->getEdges().size() - 1)].getSource();
         }
+        std::cout<< "Selected source vertex: " << sourceVertex  << std::endl;
 
-        sampledVertices.insert(sourceVertex);
+        std::queue<long long> queue;
 
-        // Loop through the vertex' neighbors, add the target vertex and edge to the sampled graph
-        for (int i = 0; i < adjacencyList[sourceVertex].size(); i++) {
+        queue.push(sourceVertex);
 
-            long long targetVertex = adjacencyList[sourceVertex][i];
+        long long desiredNumberOfEdges = (isUndirected) ? graph->getEdges().size() * 2 * fraction : graph->getEdges().size() * fraction;
 
+        std::unordered_map<long long, std::vector<long long>> adjacencyList = graph->getAdjacencyList();
 
-            //sampledVertices.insert(targetVertex);
-            queue.push(targetVertex);
+        assert(!queue.empty());
 
+        while (!queue.empty() && !reachedDesiredNumberOfEdges) {
+            long long sourceVertex = queue.front();
+            queue.pop();
 
-            std::cout<< sourceVertex << ", " << targetVertex<< std::endl;
-
-
-            if (sampledEdges.size() >= 50) {
-                reachedDesiredNumberOfEdges = true;
-                break;
+            if (sampledVertices.count(sourceVertex)) {
+                continue;
             }
 
-            sampledEdges.push_back(Edge<long long>(sourceVertex, targetVertex));
+            sampledVertices.insert(sourceVertex);
 
-            // if  (isUndirected) {
-            //    sampledEdges.push_back(Edge<long long>(targetVertex, sourceVertex));
-            //}
+            // Loop through the vertex' neighbors, add the target vertex and edge to the sampled graph
+            for (int i = 0; i < adjacencyList[sourceVertex].size(); i++) {
+
+                long long targetVertex = adjacencyList[sourceVertex][i];
+
+                queue.push(targetVertex);
+
+                if (sampledEdges.size() >= desiredNumberOfEdges) {
+                    reachedDesiredNumberOfEdges = true;
+                    break;
+                }
+
+                sampledEdges.push_back(Edge<long long>(sourceVertex, targetVertex));
+            }
         }
+
+        sourceVertex = graph->getEdges()[getRandomIntBetweenRange(0, graph->getEdges().size() - 1)].getSource();
+
     }
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
